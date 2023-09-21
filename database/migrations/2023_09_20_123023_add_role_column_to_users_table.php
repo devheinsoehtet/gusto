1417156811676a2dffa9459e5f993584a5c5a6a4
Hein <cantatasonatadev@gmail.com>
2023-09-21 13:52:02 +0630

    add role permission

---
+++ database/migrations/2023_09_20_123023_add_role_column_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->default(2);
+
+            $table->foreign('role_id', 'user_role_id_foreign')->references('id')->on('roles')->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('user_role_id_foreign');
+            $table->dropColumn('role_id');
+        });
+    }
+};
