9892157a63d4be25283306dc1e09e843572c8190
Hein <cantatasonatadev@gmail.com>
2023-09-23 21:50:10 +0630

    add bootstrap toast

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,8 +2,8 @@
 import * as Popper from '@popperjs/core'
 window.Popper = Popper
 
-import 'bootstrap';
-
+import * as bootstrap from 'bootstrap';
+window.Toast = bootstrap.Toast;
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,7 +16,9 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -23,7 +26,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -31,7 +36,8 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="{{ route('dashboard.index') }}">Dashboard</a>
+                            <a class="nav-link active" aria-current="page"
+                                href="{{ route('dashboard.index') }}">Dashboard</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" aria-current="page" href="{{ route('cars.index') }}">Cars</a>
@@ -55,13 +61,14 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
@@ -80,6 +87,40 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+        <div class="toast-container position-fixed bottom-0 end-0 p-3">
+            <div class="toast" role="alert" id="toastAlert">
+                <div class="toast-body">
+                </div>
+            </div>
+        </div>
     </div>
+
+    <script type="module">
+        const actionStatus = {{ Js::from($actionStatus ?? null) }};;
+        const actionMessage = {{ Js::from($actionMessage ?? null) }};
+
+        if (actionStatus && actionMessage) {
+            const eToastAlert = document.getElementById('toastAlert');
+
+            const toastAlert = Toast.getOrCreateInstance(eToastAlert);
+            switch (actionStatus) {
+                case 'success':
+                    eToastAlert.classList.add('text-bg-success');
+                    break;
+                case 'warn':
+                    eToastAlert.classList.add('text-bg-warning');
+                    break;
+                case 'fail':
+                    eToastAlert.classList.add('text-bg-danger');
+                    break;
+            }
+            eToastAlert.querySelector('.toast-body').innerHTML = actionMessage;
+            toastAlert.show();
+        }
+    </script>
 </body>
+
+
+
 </html>
