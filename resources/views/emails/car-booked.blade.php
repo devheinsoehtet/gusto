3dd69ff893a5f931d02313acd6bf79ff0712e47c
Hein <cantatasonatadev@gmail.com>
2023-09-26 21:57:32 +0630

    add template to mail

--- resources/views/emails/car-booked.blade.php
+++ resources/views/emails/car-booked.blade.php
@@ -1,5 +1,17 @@
-{{-- <style>
+<h2>Booking Confirmation</h2>
+<p>Dear {{ auth()->user()->name}}</p>
+<p>We are pleased to inform you that your booking has been successfully completed. Below are the details of your booking:</p>
 
-</style> --}}
+<p><strong>Here is your booking link</strong> {{ route('bookings.show', $booking->id) }}</p>
 
-<p>Laravel Email Testing</p>
+<p><strong>Booking ID:</strong>{{ $booking->id }}</p>
+<p><strong>Start Date:</strong> {{ $booking->start_date }}</p>
+<p><strong>End Date:</strong> {{ $booking->end_date }}</p>
+<p><strong>Amount:</strong> {{ $booking->amount }}</p>
+<p><strong>Duration:</strong> {{ $booking->duration }} days</p>
+
+<img src="{{ asset($car->img_url) }}" style="max-width: 100px;">
+
+<p>Thank you for choosing our service. If you have any questions or need further assistance, please feel free to contact us.</p>
+
+<p>Best regards,<br>Your Booking Team</p>
